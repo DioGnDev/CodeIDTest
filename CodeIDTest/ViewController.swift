be3090d76b6eef83122bd964025c0501f66341c4
DioGnDev <guitardio90@gmail.com>
2025-09-26 10:55:26 +0700

    first commit

---
+++ CodeIDTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeIDTest
+//
+//  Created by Ilham Prabawa on 26/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
